3ad2e5860c62170637b1fa7f1cd012f385dc792e
Kalam031 <abukalam973@gmail.com>
2020-12-26 10:48:54 +0600

    testing news api provider

---
+++ test/src/resources/news_api_provider_test.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+import 'package:test/test.dart';
+import 'package:http/http.dart';
+import 'package:http/testing.dart';
+import 'package:news/src/resources/news_api_provider.dart';
+
+void main() {
+  test('fetchTopIds returns a list of ids', () async {
+    final newsApi = NewsApiProvider();
+
+    newsApi.client = MockClient((request) async {
+      return Response(json.encode([1, 2, 3, 4]), 200);
+    });
+
+    final ids = await newsApi.fetchTopIds();
+
+    expect(ids, [1, 2, 3, 4]);
+  });
+
+  test('FetchItem returns a item model', () async {
+    final newsApi = NewsApiProvider();
+
+    newsApi.client = MockClient((request) async {
+      final jsonMap = {'id': 123};
+      return Response(json.encode(jsonMap), 200);
+    });
+
+    final item = await newsApi.fetchItems(999);
+
+    expect(item.id, 123);
+  });
+}
